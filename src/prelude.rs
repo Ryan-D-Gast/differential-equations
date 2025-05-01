9ee73ccf70660c99259a120ac272931b9c753e84
Ryan-D-Gast <ryguy10202003@gmail.com>
2025-04-30 19:16:07 -0500

    Update prelude.rs

--- src/prelude.rs
+++ src/prelude.rs
@@ -5,16 +5,16 @@
 pub use crate::{
     // Ordinary Differential Equations (ODE) module
     ode::{
-        ODE, // Define the ODE system
+        ODE,        // Define the ODE system
         ODEProblem, // Create IVP to solve
 
         // Re-exporting popular ODE solvers
         methods::{
             DOP853, // Adaptive Step Dormand-Prince 8(5,3) NumericalMethod with dense output of order 7
             DOPRI5, // Adaptive Step Dormand-Prince 5(4) NumericalMethod
-            RK4,   // Fixed Step Runge-Kutta 4th Order NumericalMethod
-            RKV65, // Verner 6(5) adaptive method with dense output of order 5
-            RKV98, // Verner 9(8) adaptive method with dense output of order 9  RKV98,
+            RK4,    // Fixed Step Runge-Kutta 4th Order NumericalMethod
+            RKV65,  // Verner 6(5) adaptive method with dense output of order 5
+            RKV98,  // Verner 9(8) adaptive method with dense output of order 9  RKV98,
         },
     },
 
@@ -25,9 +25,9 @@ pub use crate::{
 
         // Re-exporting popular SDE solvers
         methods::{
-            EM,
-            Milstein,
-            RKM4,
+            EM,       // Euler-Maruyama method (strong order 0.5)
+            Milstein, // Milstein method (strong order 1.0)
+            RKM4,     // Stochastic Runge-Kutta Maruyama 4
         },
     },
 
