d454745672e5872a8ffbd6092cb22d1116d4021c
Ryan-D-Gast <ryguy10202003@gmail.com>
2025-05-05 15:42:59 -0500

    Update mod.rs

--- src/ode/methods/runge_kutta/implicit/mod.rs
+++ src/ode/methods/runge_kutta/implicit/mod.rs
@@ -1,13 +1,13 @@
-// Implicit Runge-Kutta methods
+// Implicit Runge-Kutta methods without jacobian using fixed point iteration instead.
 
 mod fixed_step;
 pub use fixed_step::{
-    BackwardEuler, // Backward Euler method (1st order Runge-Kutta)
-    CrankNicolson, // Crank-Nicolson method (2nd order Runge-Kutta)
+    BackwardEuler, // Backward Euler method 1st order implicit Runge-Kutta
+    CrankNicolson, // Crank-Nicolson method 2nd order implicit Runge-Kutta
 };
 
 mod adaptive_step;
 pub use adaptive_step::{
-    GaussLegendre4, // Gauss-Legendre 4th order method (implicit Runge-Kutta)
-    GaussLegendre6, // Gauss-Legendre 6th order method (implicit Runge-Kutta)
+    GaussLegendre4, // Gauss-Legendre 4th order method implicit Runge-Kutta
+    GaussLegendre6, // Gauss-Legendre 6th order method implicit Runge-Kutta
 };
